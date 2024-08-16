27e37813b1f28be33a638841720f4a46f74138b9
Mateie <mateie@email..com>
2024-08-16 00:34:17 -0700

    more features

--- src/resolvers/channels.ts
+++ src/resolvers/channels.ts
@@ -4,9 +4,11 @@ import ChannelSchema from "schemas/servers/Channel";
 import ServerSchema from "schemas/servers/Server";
 import { GraphQLError } from "graphql";
 import { Snowflake } from "@theinternetfolks/snowflake";
+import MessageSchema from "schemas/servers/Message";
 
 enum ChannelEvents {
-    ChannelCreated = "CHANNEL_CREATED"
+    ChannelCreated = "CHANNEL_CREATED",
+    MessageCreated = "MESSAGE_CREATED"
 }
 
 export default {
@@ -49,6 +51,45 @@ export default {
             return ChannelSchema.findOne({
                 id,
                 server: serverId
+            });
+        },
+        getMessages: async (
+            _: any,
+            { serverId, channelId }: { serverId: string; channelId: string }
+        ) => {
+            const server = await ServerSchema.findOne({ id: serverId });
+            if (!server)
+                throw new GraphQLError("Server not found.", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "server",
+                                message: "Server not found."
+                            }
+                        ]
+                    }
+                });
+
+            const channel = await ChannelSchema.findOne({
+                id: channelId,
+                server: serverId
+            });
+
+            if (!channel)
+                throw new GraphQLError("Channel not found.", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "channel",
+                                message: "Channel not found."
+                            }
+                        ]
+                    }
+                });
+
+            return MessageSchema.find({
+                server: serverId,
+                channel: channelId
             });
         }
     },
@@ -94,6 +135,85 @@ export default {
             });
 
             return channel;
+        },
+        createMessage: async (
+            _: any,
+            {
+                serverId,
+                channelId,
+                content
+            }: {
+                serverId: string;
+                channelId: string;
+                content: string;
+            },
+            { user }: { user: any }
+        ) => {
+            if (content.length < 1 || content.length > 2000)
+                throw new GraphQLError(
+                    "Message must be between 1 and 2000 characters.",
+                    {
+                        extensions: {
+                            errors: [
+                                {
+                                    type: "message",
+                                    message:
+                                        "Message must be between 1 and 2000 characters."
+                                }
+                            ]
+                        }
+                    }
+                );
+
+            const server = await ServerSchema.findOne({ id: serverId });
+            if (!server)
+                throw new GraphQLError("Server not found.", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "server",
+                                message: "Server not found."
+                            }
+                        ]
+                    }
+                });
+
+            const channel = await ChannelSchema.findOne({
+                id: channelId,
+                server: serverId
+            });
+
+            if (!channel)
+                throw new GraphQLError("Channel not found.", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "channel",
+                                message: "Channel not found."
+                            }
+                        ]
+                    }
+                });
+
+            const message = new MessageSchema({
+                id: Snowflake.generate(),
+                server: server.id,
+                channel: channel.id,
+                member: user.id,
+                content,
+                createdAt: new Date(),
+                createdTimestamp: Date.now()
+            });
+
+            channel.messages.push(message.id);
+
+            await message.save();
+
+            await pubsub.publish(ChannelEvents.MessageCreated, {
+                messageCreated: message
+            });
+
+            return message;
         }
     },
     Subscription: {
@@ -103,6 +223,19 @@ export default {
                 async (payload, _, { user }) => {
                     const server = await ServerSchema.findOne({
                         id: payload.channelCreated.server
+                    });
+                    if (!server) return false;
+
+                    return server.members.includes(user.id);
+                }
+            )
+        },
+        messageCreated: {
+            subscribe: withFilter(
+                () => pubsub.asyncIterator(ChannelEvents.MessageCreated),
+                async (payload, _, { user }) => {
+                    const server = await ServerSchema.findOne({
+                        id: payload.messageCreated.server
                     });
                     if (!server) return false;
 
