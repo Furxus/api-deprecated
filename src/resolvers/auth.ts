6a8062cec755b0bd56cd2da29a98168fbab2ae7d
Mateie <contact@mateie.dev>
2024-08-21 21:26:12 -0700

    Fixed an api issue

--- src/resolvers/auth.ts
+++ src/resolvers/auth.ts
@@ -290,7 +290,7 @@ export default {
 
             return {
                 token: encrypt(user.generateToken()),
-                ...user
+                ...user.toJSON()
             };
         }
     }
