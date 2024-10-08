2d6b7e3f9478b42ab672ec8aad872d544dd9a255
Mateie <business@mateie.dev>
2024-10-07 22:06:07 -0700

    added some api endpoints for friends

--- src/resolvers/friends/friends.ts
+++ src/resolvers/friends/friends.ts
@@ -79,50 +79,199 @@ export default {
                     }
                 });
 
+            // Store the friend request that the user has sent to user 2
+            if (!userDoc.friendRequests?.sent.includes(userId)) {
+                userDoc.friendRequests?.sent.push(userId);
+            }
+
+            // Store the friend request that user 2 has received from user 1
+            if (!friendDoc.friendRequests?.received.includes(user.id)) {
+                friendDoc.friendRequests?.received.push(user.id);
+            }
+
+            // Check if user 2 has sent a friend request to user 1, if so make them friends
             if (
-                userDoc.friendRequests?.sent.includes(userId) &&
+                userDoc.friendRequests?.received.includes(userId) &&
                 friendDoc.friendRequests?.sent.includes(user.id)
             ) {
                 userDoc.friends.push(userId);
                 friendDoc.friends.push(user.id);
 
+                userDoc.friendRequests.received =
+                    userDoc.friendRequests?.received.filter(
+                        (friend) => friend !== userId
+                    );
+                friendDoc.friendRequests.sent =
+                    friendDoc.friendRequests?.sent.filter(
+                        (friend) => friend !== user.id
+                    );
+            }
+
+            // Check if user 1 has sent a friend request to user 2, if so make them friends
+            if (
+                friendDoc.friendRequests?.received.includes(user.id) &&
+                userDoc.friendRequests?.sent.includes(userId)
+            ) {
+                userDoc.friends.push(userId);
+                friendDoc.friends.push(user.id);
+
+                userDoc.friendRequests.sent =
+                    userDoc.friendRequests?.sent.filter(
+                        (friend) => friend !== userId
+                    );
+                friendDoc.friendRequests.received =
+                    friendDoc.friendRequests?.received.filter(
+                        (friend) => friend !== user.id
+                    );
+            }
+
+            // Save the changes
+            await userDoc.save();
+            await friendDoc.save();
+
+            return true;
+        },
+        acceptFriendRequest: async (
+            _: any,
+            { userId }: { userId: string },
+            { user }: { user: User }
+        ) => {
+            const userDoc = await UserSchema.findOne({
+                id: user.id
+            });
+
+            if (!userDoc)
+                throw new GraphQLError("User not found", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "user",
+                                message: "User not found"
+                            }
+                        ]
+                    }
+                });
+
+            const friendDoc = await UserSchema.findOne({
+                id: userId
+            });
+
+            if (!friendDoc)
+                throw new GraphQLError("Friend not found", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "user",
+                                message: "Friend not found"
+                            }
+                        ]
+                    }
+                });
+
+            if (userDoc.friends.includes(userId))
+                throw new GraphQLError("Already friends", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "user",
+                                message: "Already friends"
+                            }
+                        ]
+                    }
+                });
+
+            // Make them friends
+            userDoc.friends.push(userId);
+            friendDoc.friends.push(user.id);
+
+            // Check if user 1 has received a friend request to user 2, if so remove it
+            // Check if user 1 has sent a friend request to user 2, if so remove it
+            if (userDoc.friendRequests) {
+                userDoc.friendRequests.received =
+                    userDoc.friendRequests.received.filter(
+                        (friend) => friend !== userId
+                    );
+
                 userDoc.friendRequests.sent =
                     userDoc.friendRequests.sent.filter(
                         (friend) => friend !== userId
                     );
+            }
+
+            // Check if user 2 has received a friend request to user 1, if so remove it
+            // Check if user 2 has sent a friend request to user 1, if so remove it
+            if (friendDoc.friendRequests) {
+                friendDoc.friendRequests.received =
+                    friendDoc.friendRequests?.sent.filter(
+                        (friend) => friend !== user.id
+                    );
+                friendDoc.friendRequests.sent =
+                    friendDoc.friendRequests?.sent.filter(
+                        (friend) => friend !== user.id
+                    );
+            }
+
+            await userDoc.save();
+            await friendDoc.save();
+
+            return true;
+        },
+        declineFriendRequest: async (
+            _: any,
+            { userId }: { userId: string },
+            { user }: { user: User }
+        ) => {
+            const userDoc = await UserSchema.findOne({
+                id: user.id
+            });
+
+            if (!userDoc)
+                throw new GraphQLError("User not found", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "user",
+                                message: "User not found"
+                            }
+                        ]
+                    }
+                });
+
+            const friendDoc = await UserSchema.findOne({
+                id: userId
+            });
+
+            if (!friendDoc)
+                throw new GraphQLError("Friend not found", {
+                    extensions: {
+                        errors: [
+                            {
+                                type: "user",
+                                message: "Friend not found"
+                            }
+                        ]
+                    }
+                });
+
+            if (userDoc.friendRequests?.received.includes(userId)) {
+                userDoc.friendRequests.received =
+                    userDoc.friendRequests.received.filter(
+                        (friend) => friend !== userId
+                    );
+            }
+
+            if (friendDoc.friendRequests?.sent.includes(user.id)) {
                 friendDoc.friendRequests.sent =
                     friendDoc.friendRequests.sent.filter(
                         (friend) => friend !== user.id
                     );
-
-                await userDoc.save();
-                await friendDoc.save();
-
-                return true;
             }
 
-            if (!userDoc.friendRequests?.sent.includes(userId)) {
-                userDoc.friendRequests?.sent.push(userId);
-                await userDoc.save();
-            }
-
-            if (!friendDoc.friendRequests?.received.includes(user.id)) {
-                friendDoc.friendRequests?.received.push(user.id);
-                await friendDoc.save();
-            }
+            await userDoc.save();
+            await friendDoc.save();
 
             return true;
         },
-        acceptFriendRequest: (
-            _: any,
-            { userId }: { userId: string },
-            { user }: { user: User }
-        ) => {},
-        declineFriendRequest: (
-            _: any,
-            { userId }: { userId: string },
-            { user }: { user: User }
-        ) => {},
         removeFriend: async (
             _: any,
             { userId }: { userId: string },
